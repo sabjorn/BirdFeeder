f8ba401fdff61dab39f86ef81414bdd2e3b970a5
Steven A. Bjornson <info@sabjorn.net>
2016-01-10 12:36:21 -0800

    added hysterisis to Ultrasonic class. Should prevent flickering.

--- src/ultrasonic/Ultrasonic.py
+++ src/ultrasonic/Ultrasonic.py
@@ -3,12 +3,16 @@ import time
 import sys
 
 class Ultrasonic:
-	def __init__(self, trig, echo, thresh):
+	def __init__(self, trig, echo, thresh_low, thersh_high):
 		""" Initiate ultrasonic Range """
 		self.echo = echo
 		self.trig = trig
-		self.thresh = thresh
-		self.dist = 0
+		self.thresh_low = thresh_low
+		self.thresh_high = thresh_high
+		
+		self.hist_flag = 0 #historesis flag
+		self.dist = 0 #distance storage
+
 
 		##setup GPIO
 		GPIO.setup(self.echo, GPIO.IN)
@@ -39,12 +43,21 @@ class Ultrasonic:
 		return self.dist
 
 	def threshold(self):
-		if self.distance() < self.thresh:
+		if self.distance() < self.thresh_low:
 			return 1
 		else:
 			return 0
 
+	#if the value is below the thesh_low, flag HIGH.
+	# once this flag is HIGH, if the value goes above thresh_high, flag LOW
+	def hyst(self):
+		if (self.distance() < self.thresh_low and not self.hist_flag):
+			self.hist_flag = 1
+		elif (self.distance > self.thresh_high and self.hist_flag):
+			self.hist_flag = 0
+
+		return self.hist_flag
+
 	def close(self):
         	""" Graceful shutdown """ 
         	GPIO.cleanup()
-
